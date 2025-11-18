53fcd8c3a9174b9b158381b46967ff4385ae2b1b
PigeonsHouse <bullbull1230@gmail.com>
2025-11-18 23:30:08 +0900

    update: plugin

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,8 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import generouted from "@generouted/react-router/plugin";
 import { generateCharactersList } from "./vite-plugins/generate-characters-list";
+import netlify from "@netlify/vite-plugin";
 
 export default defineConfig({
-  plugins: [react(), generouted(), generateCharactersList()],
+  plugins: [react(), generouted(), generateCharactersList(), netlify()],
 });
