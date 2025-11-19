c7b15f215f6c0a4417bf8499dd46d5d033742da4
PigeonsHouse <bullbull1230@gmail.com>
2025-11-19 10:02:28 +0900

    update: netlify redirects

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import generouted from "@generouted/react-router/plugin";
 import { generateCharactersList } from "./vite-plugins/generate-characters-list";
-import netlify from "@netlify/vite-plugin";
 
 export default defineConfig({
-  plugins: [react(), generouted(), generateCharactersList(), netlify()],
+  plugins: [react(), generouted(), generateCharactersList()],
 });
