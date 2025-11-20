ed224b5425e281ccb693f24a04b7727ce92e0240
PigeonsHouse <bullbull1230@gmail.com>
2025-11-20 14:45:17 +0900

    fix: spページ仮組み

--- src/pages/characters/[name]/_sp.tsx
+++ src/pages/characters/[name]/_sp.tsx
@@ -1,4 +1,6 @@
-import type { CharacterDetail } from "../../../types";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router";
+import type { CharacterDetail, CharacterSummary } from "../../../types";
 
 type SpCharacterAboutProps = {
   characterId: string;
@@ -9,6 +11,51 @@ export const SpCharacterAbout: React.FC<SpCharacterAboutProps> = ({
   characterId,
   data,
 }) => {
+  const navigate = useNavigate();
+
+  const [summary, setSummary] = useState<CharacterSummary[]>([]);
+  const [previousCharacterId, setPreviousCharacterId] = useState<
+    string | undefined
+  >();
+  const [nextCharacterId, setNextCharacterId] = useState<string | undefined>();
+  const [displaySpriteIndex, setDisplaySpriteIndex] = useState(0);
+
+  useEffect(() => {
+    fetch(`${import.meta.env.BASE_URL}data/characters.json`)
+      .then((res) => res.json())
+      .then((data) => setSummary(data));
+  }, []);
+  useEffect(() => {
+    let prevId: string | undefined = undefined;
+    let nextId: string | undefined = undefined;
+    let isFind = false;
+    summary.map((summaryData) => {
+      if (summaryData.id === characterId) {
+        isFind = true;
+      } else if (isFind && nextId === undefined) {
+        nextId = summaryData.id;
+      }
+      if (!isFind) {
+        prevId = summaryData.id;
+      }
+    });
+    setPreviousCharacterId(prevId);
+    setNextCharacterId(nextId);
+  }, [data, summary]);
+
+  const handlePrevious = previousCharacterId
+    ? () => {
+        setDisplaySpriteIndex(0);
+        navigate(`/characters/${previousCharacterId}`);
+      }
+    : undefined;
+  const handleNext = nextCharacterId
+    ? () => {
+        setDisplaySpriteIndex(0);
+        navigate(`/characters/${nextCharacterId}`);
+      }
+    : undefined;
+
   return (
     <div
       style={{
@@ -26,7 +73,17 @@ export const SpCharacterAbout: React.FC<SpCharacterAboutProps> = ({
           backgroundColor: data.colorPalette[0],
         }}
       >
+        <div onClick={handlePrevious} style={{ height: 30 }}>
+          {previousCharacterId ? "←前へ" : ""}
+        </div>
         <h1 style={{ margin: 0 }}>{data.name}</h1>
+        <div onClick={handleNext} style={{ height: 30 }}>
+          {nextCharacterId ? "次へ→" : ""}
+        </div>
+        <img
+          src={data.spritesUrl[displaySpriteIndex]}
+          style={{ width: "100%", objectFit: "cover" }}
+        />
       </div>
     </div>
   );
