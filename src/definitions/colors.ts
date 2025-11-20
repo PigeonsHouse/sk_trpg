7d7e26c96777d9d8cfa2726272e426e767af4140
PigeonsHouse <bullbull1230@gmail.com>
2025-11-20 22:45:01 +0900

    fix: 使ってない変数のコメントアウト

--- src/definitions/colors.ts
+++ src/definitions/colors.ts
@@ -1,23 +1,23 @@
-// 案内看板の枠線の色
-const gray5 = "#5A5A5A";
-// 案内看板の■の色
-const gray1 = "#313131";
+// // 案内看板の枠線の色
+// const gray5 = "#5A5A5A";
+// // 案内看板の■の色
+// const gray1 = "#313131";
 
-// 左上看板の枠線の色
-const gray2 = "#3E3E3E";
-// 左上看板の中の色
-const gray7 = "#EFEFEF";
+// // 左上看板の枠線の色
+// const gray2 = "#3E3E3E";
+// // 左上看板の中の色
+// const gray7 = "#EFEFEF";
 
-// about各見出しの色
-const gray3 = "#444444";
+// // about各見出しの色
+// const gray3 = "#444444";
 
-// 名前枠の枠線の色
-const gray6 = "#D2D2D2";
-// 位置座標マークの色
-const gray4 = "#4B4B4B";
+// // 名前枠の枠線の色
+// const gray6 = "#D2D2D2";
+// // 位置座標マークの色
+// const gray4 = "#4B4B4B";
 
-// 信号機の色
-const black = "#272727";
+// // 信号機の色
+// const black = "#272727";
 
 export const UiColor = {
   black: "#1A1A1A",
