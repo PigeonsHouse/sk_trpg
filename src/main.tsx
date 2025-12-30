9ad206b9a289e17fc65a4607468d9b59ad54f9d4
PigeonsHouse <bullbull1230@gmail.com>
2025-12-30 14:40:00 +0900

    update: キャッシュが効いてなかったので修正

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,14 @@ import { Routes } from "@generouted/react-router";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { GlobalStyles } from "./components";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: Infinity,
+      refetchOnWindowFocus: false,
+    },
+  },
+});
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
